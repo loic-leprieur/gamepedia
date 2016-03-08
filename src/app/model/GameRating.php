3733f31731bce8193ff5e38fb66b36e8e7a1d58b
MorganDbs <mailmorgandubois@gmail.com>
2016-03-08 11:48:09 +0100

    dfskjbk

---
+++ src/app/model/GameRating.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loic
+ * Date: 22/02/16
+ * Time: 14:56
+ */
+
+namespace app\model;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class GameRating extends Model
+{
+    /**
+     * Table pochette de la base de données
+     * @var string Nom de la table pochette
+     */
+    protected $table = 'game_rating';
+
+    /**
+     * Clé primaire dans la table pochette
+     * @var string Clé primaire de la table pochette
+     */
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+}
